0ed20f83a81b1c1971b340b6f7906c55962f37cb
viresh rawool <vireshrawool007@gmail.com>
2021-02-06 23:17:17 +0530

    added the eclipse and intellj work

---
+++ eclipse/JavaPratices/src/main/java/com/javapratices/App.java
@@ -0,0 +1,23 @@
+package com.javapratices;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+			
+    public static void main( String[] args )
+    {
+		String message = "hello, world";
+		LOG.debug(message + "will be printed on debug");
+		LOG.info(message + "will be printed on info");
+		LOG.warn(message + "will be printed on warn");
+		LOG.error(message + "will be printed on error");
+		LOG.fatal(message + "will be printed on fatal");
+		LOG.info("Appending string: {}.",message );
+        System.out.println( message );
+    }
+}
---
+++ eclipse/helloworld/src/helloworld/newclass.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class newclass {
+	public static void main(String[] args ) {
+		System.out.println("welcome to java");
+	}
+
+}
